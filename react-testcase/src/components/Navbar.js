a244ded35e41155a4a9c777568f05705eec1e91d
Sofia Lindeberg <sofy.ber@hotmail.com>
2021-11-11 22:58:25 +0100

    Navbar Component

--- react-testcase/src/components/Navbar.js
+++ react-testcase/src/components/Navbar.js
@@ -2,15 +2,22 @@
 import React,{useState} from 'react'; 
 //import {Link} from 'react-router-dom'; 
 import './Navbar.css'; 
+import IMDBLogo from '../media/IMDBLogo.png'; 
 
 function Navbar() { 
     return (
         <> 
-        <nav className='navbar'> 
-            
-          
+        <div className='navWrapper'>
+            <a href='google.com'> <img className='navIMDBLink' src={IMDBLogo} alt ="IMDB LOGO">
+                </img>
+        </a>
+           <nav className='navbar'>
+                
+            <a className='moviesLink' href="google.com">Movies</a>
+                                                           
         
         </nav>
+        </div>
         </> 
         ); 
 }
