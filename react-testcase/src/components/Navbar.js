b85cb8d82306aafb002432b7abd2341884eca1c4
Sofia Bernalsson <sofy.ber@hotmail.com>
2021-11-11 17:23:36 +0100

    First component added

---
+++ react-testcase/src/components/Navbar.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line 
+import React,{useState} from 'react'; 
+//import {Link} from 'react-router-dom'; 
+
+function Navbar() { 
+    return (
+        <> 
+        <nav className="navbar"> 
+           <div className="navbar-container">
+        
+        <p> Ahhhhhh </p>
+        
+            </div> 
+        
+        </nav>
+        </> 
+        ); 
+}
+
+export default Navbar
