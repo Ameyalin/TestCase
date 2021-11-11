f8f35f4d963f9620c641450389278e5a11e777b3
Sofia Bernalsson <sofy.ber@hotmail.com>
2021-11-11 17:19:42 +0100

    Added first component

--- react-testcase/src/App.js
+++ react-testcase/src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> yo </h1>
+    return ( <div className = 'App'>
+        <Navbar />
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
